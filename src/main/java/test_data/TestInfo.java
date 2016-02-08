ca7ec0e5fcf5a3713cf70d90d520c1f8504370ce
orestisMelkonian <sdi1000128@di.uoa.gr>
2016-02-08 18:14:11 +0200

    TestInfo now stores result queues for displaying later

--- src/main/java/test_data/TestInfo.java
+++ src/main/java/test_data/TestInfo.java
@@ -3,14 +3,15 @@ package test_data;
 import org.reactive_ros.Stream;
 
 import java.util.Arrays;
+import java.util.Queue;
 
 /**
  * @author Orestis Melkonian
  */
 public class TestInfo {
     public String name;
-    public Stream s1;
-    public Stream s2;
+    public Stream s1, s2;
+    public Queue q1, q2;
 
     public TestInfo(String name, Stream s1, Stream s2) {
         this.name = name;
@@ -19,10 +20,9 @@ public class TestInfo {
     }
 
     public boolean equality() {
-        return Arrays.equals(
-                s1.toBlocking().toQueue().toArray(),
-                s2.toBlocking().toQueue().toArray()
-        );
+        q1 = s1.toBlocking().toQueue();
+        q2 = s2.toBlocking().toQueue();
+        return Arrays.equals(q1.toArray(), q2.toArray());
 //        return (boolean) Stream.sequenceEqual(s1, s2).toBlocking().first();
     }
 }
